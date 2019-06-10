6e7a534ebbfe92a2825247f1ab2bf3e0292a8043
Montana <montana.harkin@gmail.com>
2019-06-10 00:52:32 -0700

    Got some data reading going.

--- test-watch-detection.js
+++ test-watch-detection.js
@@ -3,11 +3,18 @@ usbDetect.startMonitoring();
 
 const NIKE_VENDOR_ID = 4524;
 const NIKE_SPORTWATCH_PRODUCT_ID = 21588;
+const NIKE_SPORTWATCH_PRODUCT_ID_2 = 21589;
 
 usbDetect.on(`add:${NIKE_VENDOR_ID}:${NIKE_SPORTWATCH_PRODUCT_ID}`, function(device) {
 	console.log("Nike Sportwatch GPS Plugged In");
 });
-
 usbDetect.on('remove', function(device) {
 	console.log("Nike Sportwatch GPS Unplugged");
+});
+
+usbDetect.on(`add:${NIKE_VENDOR_ID}:${NIKE_SPORTWATCH_PRODUCT_ID_2}`, function(device) {
+	console.log("Nike Sportwatch GPS 2 Plugged In");
+});
+usbDetect.on(`remove:${NIKE_VENDOR_ID}:${NIKE_SPORTWATCH_PRODUCT_ID_2}`, function(device) {
+	console.log("Nike Sportwatch GPS 2 Unplugged");
 });
